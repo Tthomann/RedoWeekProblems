57daf5973428c28cdc88da14715d2d869eeb3c36
Tyler Thomann <Tyler Thomann>
2016-04-12 17:15:40 -0500

    operater+ overload problem finished

---
+++ Problem1/Problem1/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    class Person
+    {
+        public string name;
+        public string lastName;
+        public int age;
+        public string birthday;
+       
+        public Person(string Birthday)
+        {
+            birthday = Birthday;
+         DateTime currentDateTime = DateTime.Today;
+        DateTime birthDateTime = DateTime.Parse(Birthday);
+
+        age = currentDateTime.Year - birthDateTime.Year;
+        }
+        public static Person operator +(Person informationA, int informationB)
+        {
+            informationA.age = informationA.age + informationB;
+      
+            return informationA;
+        }
+    }
+}
---
+++ Problem1/Problem1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("01/04/1996");
+            person += 12; 
+            Console.WriteLine(person.age);
+            Console.ReadLine();
+        }
+    }
+}
