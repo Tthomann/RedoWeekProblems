761a5bc275c07d4ea2bb69d717a171ae1623fc62
Tyler Thomann <Tyler Thomann>
2016-04-13 14:37:56 -0500

    enum problem complete

---
+++ enumProblem/enumProblem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WeekForcast days = new WeekForcast();
+            days.displayForcast();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ enumProblem/enumProblem/WeekForcast.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumProblem
+{
+    class WeekForcast
+    {
+        enum daysOfTheWeek { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday }
+        enum weatherForTheWeek { Sunny, Rain, Storms, Overcast, Cloudy, Windy, Clear}
+        public WeekForcast()
+        {
+            
+        }
+          public void displayForcast()
+        {
+            Console.WriteLine(daysOfTheWeek.Sunday + ":{0}", weatherForTheWeek.Sunny);
+            Console.WriteLine(daysOfTheWeek.Monday + ":{0}", weatherForTheWeek.Rain);
+            Console.WriteLine(daysOfTheWeek.Tuesday + ":{0}", weatherForTheWeek.Storms);
+            Console.WriteLine(daysOfTheWeek.Wednesday + ":{0}", weatherForTheWeek.Overcast);
+            Console.WriteLine(daysOfTheWeek.Thursday + ":{0}", weatherForTheWeek.Cloudy);
+            Console.WriteLine(daysOfTheWeek.Friday + ":{0}", weatherForTheWeek.Windy);
+            Console.WriteLine(daysOfTheWeek.Saturday + ":{0}", weatherForTheWeek.Clear);
+        }
+    }
+}
+       
